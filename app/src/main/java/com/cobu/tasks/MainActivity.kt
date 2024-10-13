84fc0bc57b6cb6c1e8fedcc44a16de90ab127945
who-icyy <itscobuu@gmail.com>
2024-10-13 14:28:55 +0530

    Main

--- app/src/main/java/com/cobu/tasks/MainActivity.kt
+++ app/src/main/java/com/cobu/tasks/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.cobu.tasks
 
-import ToDoListApp
+import com.cobu.tasks.ui.pages.ToDoListApp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
--- app/src/main/java/com/cobu/tasks/ui/pages/Home.kt
+++ app/src/main/java/com/cobu/tasks/ui/pages/Home.kt
@@ -1,15 +1,34 @@
-import android.app.Dialog
-import androidx.compose.foundation.layout.*
+package com.cobu.tasks.ui.pages
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.material3.TextField
-import androidx.compose.ui.Alignment
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
